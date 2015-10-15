0610cfd14d32cc8ef94b172d6064b4f500a563dd
CedSharp <cedascsharp@gmail.com>
2015-10-15 16:23:17 -0400

    Updated Task.js and added dark jquery-ui theme

--- libs/task.js
+++ libs/task.js
@@ -4,11 +4,54 @@ var DefineTask;
 
 	DefineTask = function( taskContainer ) {
 
-		$( taskContainer+" .taskList" ).sortable({
-			placeholder: "taskPlaceholder",
-			helper: "clone",
-			cursorAt: { left: 64, top: 16 },
-			connectWith: ".taskList"
+		$( taskContainer ).each( function(i,e) {
+			var $taskContainer = $(e);
+
+			$taskContainer.html(
+				'<div class="createTaskListModal" title="Create new TaskList">'+
+		          '<form>'+
+		            '<fieldset>'+
+		              '<label for="name">TaskList Name</label>'+
+		              '<input type="text" name="tasklistName" class="tasklistName" value="TODO" class="text ui-widget-content">'+
+
+		              '<!-- Allow form submission with keyboard without duplicating the dialog button -->'+
+		              '<input type="submit" tabindex="-1" style="position:absolute; top:-1000px">'+
+		            '</fieldset>'+
+		          '</form>'+
+		        '</div>'+
+				'<div class="createTaskListButton">+</div>'+
+				$taskContainer.html() +
+				'<div style="clear:both;"></div>'
+			);
+
+			$taskContainer.find(".taskList").sortable({
+				placeholder: "taskPlaceholder",
+				helper: "clone",
+				cursorAt: { left: 64, top: 16 },
+				connectWidth: ".taskList"
+			});
+
+			$taskContainer.find( "form" ).each( function( fi,fe) {
+				$form = $(fe);
+				$form.off("submit").on("submit", function(evt){
+					evt.preventDefault();
+					console.log( "Create new TaskList '"+$form.find(".tasklistName").val()+"'" );
+				});
+			});
+
+			$createTaskListModal = $taskContainer.find( ".createTaskListModal" ).dialog({
+				autoOpen: false,
+				width: 400,
+				height: 200,
+				modal: true,
+				buttons: {
+					"Create TaskList": function() {
+						console.log( "Creating tasklist '"+$(".tasklistName",$createTaskListModal).val()+"'" );
+						$createTaskListModal.dialog( "close" );
+					},
+					Cancel: function(){ $createTaskListModal.dialog( "close" ); }
+				}
+			});
 		});
 
 	};
